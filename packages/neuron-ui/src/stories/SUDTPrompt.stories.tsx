78adecce4dc9ae783df9b0e0ad026e896f2bda4b
Keith <keithwhisper@gmail.com>
2020-03-03 18:16:11 +0800

    feat: add the component of prompt above the sUDT list

---
+++ packages/neuron-ui/src/stories/SUDTPrompt.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import SUDTPrompt, { SUDTPromptProps } from 'components/SUDTPrompt'
+import { SyncStatus } from 'utils/const'
+
+const stories = storiesOf('sUDT Prompt', module)
+
+const props: { [name: string]: SUDTPromptProps } = {
+  Offline: {
+    connectionStatus: 'offline',
+    syncStatus: SyncStatus.Syncing,
+    hasItems: true,
+  },
+  'Sync not start': {
+    connectionStatus: 'online',
+    syncStatus: SyncStatus.SyncNotStart,
+    hasItems: true,
+  },
+  'Tip block not fetched': {
+    connectionStatus: 'online',
+    syncStatus: SyncStatus.FailToFetchTipBlock,
+    hasItems: true,
+  },
+  Syncing: {
+    connectionStatus: 'online',
+    syncStatus: SyncStatus.Syncing,
+    hasItems: true,
+  },
+  'Has no sUDT': {
+    connectionStatus: 'online',
+    syncStatus: SyncStatus.SyncCompleted,
+    hasItems: false,
+  },
+  'Has sUDT': {
+    connectionStatus: 'online',
+    syncStatus: SyncStatus.SyncCompleted,
+    hasItems: true,
+  },
+}
+
+Object.keys(props).forEach(key => {
+  stories.add(key, () => {
+    return <SUDTPrompt {...props[key]} />
+  })
+})
